f826d06b37c6be9bf1e16ad8af342391c2e78f22
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2020-12-22 21:29:17 +0100

    Ajout de la classe FakeDependencyInjection

---
+++ app/src/main/java/com/example/game_of_thrones/data/di/FakeDependencyInjection.java
@@ -0,0 +1,43 @@
+package com.example.game_of_thrones.data.di;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class FakeDependencyInjection {
+    private static Retrofit retrofit;
+    private static Gson gson;
+    private static Context applicationContext;
+
+    public static Retrofit getRetrofit() {
+        if (retrofit == null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("https://www.thronesapi.com/api/v2/")
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create(getGson()))
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static Gson getGson() {
+        if (gson == null) {
+            gson = new Gson();
+        }
+        return gson;
+    }
+}
