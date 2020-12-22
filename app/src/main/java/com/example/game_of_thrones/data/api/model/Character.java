f9622164a04859930ed155f42706d29d4aad0dfd
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2020-12-22 20:14:00 +0100

    Ajout des classes Character et CharacterInformation / Modification de build.gradle

---
+++ app/src/main/java/com/example/game_of_thrones/data/api/model/Character.java
@@ -0,0 +1,14 @@
+package com.example.game_of_thrones.data.api.model;
+
+public class Character {
+    private String id;
+    private CharacterInformation characterInfo;
+
+    public String getId() {
+        return id;
+    }
+
+    public CharacterInformation getCharacterInfo() {
+        return characterInfo;
+    }
+}
---
+++ app/src/main/java/com/example/game_of_thrones/data/api/model/CharacterInformation.java
@@ -0,0 +1,49 @@
+package com.example.game_of_thrones.data.api.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CharacterInformation {
+
+    @SerializedName("firstname")
+    private String firstName;
+
+    @SerializedName("lastname")
+    private String lastName;
+
+    @SerializedName("fullname")
+    private String fullName;
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("family")
+    private String family;
+
+    @SerializedName("image")
+    private String image;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+}
