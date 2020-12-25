edb81470b1541f5ef1e52a066432da5a0750339d
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2020-12-25 11:33:57 +0100

    Ajout du fragment fragment_list et son fichier xml

---
+++ app/src/main/java/com/example/game_of_thrones/presentation/display/ListFragment.java
@@ -0,0 +1,38 @@
+package com.example.game_of_thrones.presentation.display;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.game_of_thrones.R;
+
+public class ListFragment extends Fragment {
+    public ListFragment(){
+
+    }
+
+    public static ListFragment newInstance() {
+        ListFragment fragment = new ListFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_list, container, false);
+        return v;
+    }
+}
