0b9d7c07d8e18541348e0b7fe3d3740e3d80b0c9
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2020-12-27 15:27:20 +0100

    Implementation de l'utilisation du fragment et correction bugs

--- app/src/main/java/com/example/game_of_thrones/presentation/display/Main.java
+++ app/src/main/java/com/example/game_of_thrones/presentation/display/Main.java
@@ -2,14 +2,58 @@ package com.example.game_of_thrones.presentation.display;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.game_of_thrones.R;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class Main extends AppCompatActivity {
+
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
+
+    private ListFragment listFrag1 = new ListFragment();
+    private ListFragment listFrag2 = new ListFragment();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupViewPagerAndTabs();
+
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> tab.setText("FRAGMENT " + (position + 1))
+        ).attach();
+    }
+
+    private void setupViewPagerAndTabs() {
+        viewPager = findViewById(R.id.tab_viewpager);
+
+        pagerAdapter = new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                if(position == 0){
+                    return listFrag1;
+                }else{
+                    return listFrag2;
+                }
+            }
+
+            @Override
+            public int getItemCount() {
+                return 2;
+            }
+        };
+
+        viewPager.setAdapter(pagerAdapter);
     }
 }
