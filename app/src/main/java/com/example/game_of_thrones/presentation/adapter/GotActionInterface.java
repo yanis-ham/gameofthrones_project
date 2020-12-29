87fddc2678646157ab953ce8c7ad5a4b918b57cc
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2020-12-29 13:48:39 +0100

    Ajout de l'interface GotActionInterface et de la classe GotCharacterInformationViewItem

---
+++ app/src/main/java/com/example/game_of_thrones/presentation/adapter/GotActionInterface.java
@@ -0,0 +1,5 @@
+package com.example.game_of_thrones.presentation.adapter;
+
+public interface GotActionInterface {
+    void onClick(int id);
+}
---
+++ app/src/main/java/com/example/game_of_thrones/presentation/adapter/GotCharacterInformationViewItem.java
@@ -0,0 +1,59 @@
+package com.example.game_of_thrones.presentation.adapter;
+
+public class GotCharacterInformationViewItem {
+
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String title;
+    private String family;
+    private String image;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
