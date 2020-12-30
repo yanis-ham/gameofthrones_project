4672db6c6c77ac5350abd9c1ae9831dcb10e2123
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2020-12-30 14:17:04 +0100

    Ajout de la classe GotCharacterViewItem

---
+++ app/src/main/java/com/example/game_of_thrones/presentation/adapter/GotCharacterViewItem.java
@@ -0,0 +1,31 @@
+package com.example.game_of_thrones.presentation.adapter;
+
+public class GotCharacterViewItem {
+    private String id;
+    private String fullname;
+    private String img;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+}
