276da27514e74b85f1cf6e365eeef8b1513dcd50
Yanis Hamidouche <yanis.hamidouche.etu@univ-lille.fr>
2021-01-03 18:27:44 +0100

    Ajout du mapper pour favoriteFragment

---
+++ app/src/main/java/com/example/game_of_thrones/presentation/display/favorite/mapper/GotCharacterEntityToViewItemMapper.java
@@ -0,0 +1,28 @@
+package com.example.game_of_thrones.presentation.display.favorite.mapper;
+
+import com.example.game_of_thrones.data.entity.GotCharacterEntity;
+import com.example.game_of_thrones.presentation.display.favorite.adapter.FavoriteGotCharacterViewItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GotCharacterEntityToViewItemMapper {
+
+    public FavoriteGotCharacterViewItem map(GotCharacterEntity gotCharacterEntity){
+        FavoriteGotCharacterViewItem favoriteGotCharacterViewItem = new FavoriteGotCharacterViewItem();
+        favoriteGotCharacterViewItem.setId(gotCharacterEntity.getId());
+        favoriteGotCharacterViewItem.setFullname(gotCharacterEntity.getFullname());
+        favoriteGotCharacterViewItem.setImageUrl(gotCharacterEntity.getImageUrl());
+        return favoriteGotCharacterViewItem;
+    }
+
+    public List<FavoriteGotCharacterViewItem> map(List<GotCharacterEntity> gotCharacterEntityList){
+        List<FavoriteGotCharacterViewItem> favoriteList = new ArrayList<>();
+        if(gotCharacterEntityList != null){
+            for (GotCharacterEntity gotCharacterEntity : gotCharacterEntityList){
+                favoriteList.add(map(gotCharacterEntity));
+            }
+        }
+        return favoriteList;
+    }
+}
